5dd82f07c7a143c88b9a02ea4299c16165aeffde
Ron Borysovski <roni537@gmail.com>
2018-12-04 13:17:35 +0200

    feat: (WIP) Add streamer endpoint

--- src/streamer/streamer.contoller.ts
+++ src/streamer/streamer.contoller.ts
@@ -1,8 +1,78 @@
+import * as aws from 'aws-sdk';
 import { Request, Response } from 'express';
+import { config } from '../config';
 import { StreamerManager } from './streamer.manager';
+import * as fs from 'fs';
 
 export class StreamerController {
-    static async create(req: Request, res: Response) {
-        res.json(await StreamerManager.create(req.body));
+    private static s3: aws.S3 = new aws.S3({
+        accessKeyId: config.s3.accessKeyId,
+        region: config.s3.region,
+        secretAccessKey: config.s3.secretAccessKey,
+        signatureVersion: config.s3.signatureVersion,
+        endpoint: config.s3.endpoint,
+        s3ForcePathStyle: true,
+    });
+
+    static async stream(req: Request, res: Response): Promise<void> {
+        const path = req.params.path;
+        let contentLength = 0;
+
+        try {
+            contentLength = await StreamerManager.getObjectContentLength(config.s3.bucket, path, StreamerController.s3);
+        } catch (err) {
+            res.sendStatus(404).send();
+        }
+
+        // TODO: Move to validators
+        const videoNameAndSuffix = path.split('.');
+        if (videoNameAndSuffix.length === 1 || videoNameAndSuffix[1] !== 'mp4') {
+            res.sendStatus(400).send();
+        }
+
+        if (req.headers.range) {
+            const range = req.headers.range.toString();
+
+            const bytes = range.replace(/bytes=/, '').split('-');
+
+            const regEx = new RegExp(/^bytes\=\d+\-(\d+)??$/);
+
+            if (!regEx.test(range)) {
+                res.sendStatus(400).send();
+            }
+
+            let [start, end] = bytes.map((value) => {
+                return value ? parseInt(value, 10) : contentLength - 1;
+            });
+
+            if (end <= start || end > contentLength) {
+                res.sendStatus(416).send();
+            }
+
+            let CHUNKSIZE = (end - start) + 1;
+
+            // if (CHUNKSIZE > 1024 * 1024) {
+            //     end = start + 1024 * 1024 - 1;
+            //     CHUNKSIZE = (end - start) + 1;
+            // }
+
+            console.log(`bytes ${start}-${end}/${contentLength}`);
+            res.writeHead(206, {
+                'Content-Range': `bytes ${start}-${end}/${contentLength}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': CHUNKSIZE,
+                'Content-Type': 'video/mp4',
+            });
+
+            StreamerController.s3.getObject({ Key: path, Range: `bytes=${start}-${end}`, Bucket: config.s3.bucket }).createReadStream().pipe(res);
+
+        } else {
+            res.writeHead(200, {
+                'Content-Length': contentLength,
+                'Content-Type': 'video/mp4',
+            });
+
+            StreamerController.s3.getObject({ Key: path, Bucket: config.s3.bucket }).createReadStream().pipe(res);
+        }
     }
 }
--- src/streamer/streamer.manager.ts
+++ src/streamer/streamer.manager.ts
@@ -1,7 +1,8 @@
-import { IStreamer } from './streamer.interface';
+import * as aws from 'aws-sdk';
 
 export class StreamerManager {
-    static async create(streamer: IStreamer) {
-        return { created: streamer.property };
+    static async getObjectContentLength(bucket: string, path: string, s3: aws.S3): Promise<number> {
+        const result = await s3.headObject({ Bucket: bucket, Key: path }).promise();
+        return result.ContentLength!;
     }
 }
