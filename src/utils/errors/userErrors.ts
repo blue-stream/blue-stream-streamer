62825fc3549ccdbb961b90c62dad637dc782edce
Ron Borysovski <roni537@gmail.com>
2018-12-04 13:19:02 +0200

    refactor: Change user errors

--- src/utils/errors/userErrors.ts
+++ src/utils/errors/userErrors.ts
@@ -1,19 +1,13 @@
 import { UserError } from './applicationError';
 
-export class PropertyInvalidError extends UserError {
+export class PathInvalidError extends UserError {
     constructor(message?: string) {
-        super(message || `Property is invalid`, 400);
+        super(message || 'Path is invalid', 400);
     }
 }
 
-export class IdInvalidError extends UserError {
+export class ResourceNotFoundError extends UserError {
     constructor(message?: string) {
-        super(message || `Id is invalid`, 400);
-    }
-}
-
-export class StreamerNotFoundError extends UserError {
-    constructor(message?: string) {
-        super(message || `Streamer not found`, 404);
+        super(message || 'Resource not found', 404);
     }
 }
