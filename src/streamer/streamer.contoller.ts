8abe2c926143a8ad214ab951756fcee9e92a991c
Ron Borysovski <roni537@gmail.com>
2018-12-05 16:56:26 +0200

    refactor: Move logic from controller to manager

--- src/streamer/streamer.contoller.ts
+++ src/streamer/streamer.contoller.ts
@@ -1,33 +1,17 @@
-import * as aws from 'aws-sdk';
 import { Request, Response } from 'express';
-import { config } from '../config';
 import { StreamerManager } from './streamer.manager';
-import * as fs from 'fs';
+import { config } from '../config';
 
 export class StreamerController {
-    private static s3: aws.S3 = new aws.S3({
-        accessKeyId: config.s3.accessKeyId,
-        region: config.s3.region,
-        secretAccessKey: config.s3.secretAccessKey,
-        signatureVersion: config.s3.signatureVersion,
-        endpoint: config.s3.endpoint,
-        s3ForcePathStyle: true,
-    });
 
-    static async stream(req: Request, res: Response): Promise<void> {
+    static async stream(req: Request, res: Response) {
         const path = req.params.path;
         let contentLength = 0;
 
         try {
-            contentLength = await StreamerManager.getObjectContentLength(config.s3.bucket, path, StreamerController.s3);
+            contentLength = await StreamerManager.getObjectContentLength(path);
         } catch (err) {
-            res.sendStatus(404).send();
-        }
-
-        // TODO: Move to validators
-        const videoNameAndSuffix = path.split('.');
-        if (videoNameAndSuffix.length === 1 || videoNameAndSuffix[1] !== 'mp4') {
-            res.sendStatus(400).send();
+            return res.sendStatus(404).send();
         }
 
         if (req.headers.range) {
@@ -35,44 +19,39 @@ export class StreamerController {
 
             const bytes = range.replace(/bytes=/, '').split('-');
 
-            const regEx = new RegExp(/^bytes\=\d+\-(\d+)??$/);
-
-            if (!regEx.test(range)) {
-                res.sendStatus(400).send();
-            }
-
             let [start, end] = bytes.map((value) => {
                 return value ? parseInt(value, 10) : contentLength - 1;
             });
 
             if (end <= start || end > contentLength) {
                 res.sendStatus(416).send();
+                return;
             }
 
-            let CHUNKSIZE = (end - start) + 1;
+            let chunkSize = (end - start) + 1;
 
-            // if (CHUNKSIZE > 1024 * 1024) {
-            //     end = start + 1024 * 1024 - 1;
-            //     CHUNKSIZE = (end - start) + 1;
-            // }
+            if (chunkSize > config.streamer.maxChunkSize) {
+                end = start + config.streamer.maxChunkSize - 1;
+                end = Math.min(end, contentLength - 1);
+                chunkSize = config.streamer.maxChunkSize;
+            }
 
             console.log(`bytes ${start}-${end}/${contentLength}`);
             res.writeHead(206, {
                 'Content-Range': `bytes ${start}-${end}/${contentLength}`,
                 'Accept-Ranges': 'bytes',
-                'Content-Length': CHUNKSIZE,
+                'Content-Length': chunkSize,
                 'Content-Type': 'video/mp4',
             });
 
-            StreamerController.s3.getObject({ Key: path, Range: `bytes=${start}-${end}`, Bucket: config.s3.bucket }).createReadStream().pipe(res);
-
+            return StreamerManager.getObjectStream(path, `bytes=${start}-${end}`).pipe(res);
         } else {
             res.writeHead(200, {
                 'Content-Length': contentLength,
                 'Content-Type': 'video/mp4',
             });
 
-            StreamerController.s3.getObject({ Key: path, Bucket: config.s3.bucket }).createReadStream().pipe(res);
+            return StreamerManager.getObjectStream(path).pipe(res);
         }
     }
 }
--- src/streamer/streamer.manager.ts
+++ src/streamer/streamer.manager.ts
@@ -1,8 +1,23 @@
 import * as aws from 'aws-sdk';
+import { S3Instance } from '../utils/s3';
+import { config } from '../config';
 
 export class StreamerManager {
-    static async getObjectContentLength(bucket: string, path: string, s3: aws.S3): Promise<number> {
-        const result = await s3.headObject({ Bucket: bucket, Key: path }).promise();
+    static async getObjectContentLength(path: string): Promise<number> {
+        const result = await S3Instance.headObject({ Bucket: config.s3.bucket, Key: path }).promise();
         return result.ContentLength!;
+    }
+
+    static getObjectStream(path: string, range?: string) {
+        const params: aws.S3.GetObjectRequest = {
+            Key: path,
+            Bucket: config.s3.bucket
+        };
+
+        if (range) params.Range = range;
+
+        return S3Instance
+            .getObject(params)
+            .createReadStream();
     }
 }
