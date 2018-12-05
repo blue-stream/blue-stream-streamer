ec8026178c24884212c75c192d90dc9e01741313
Ron Borysovski <roni537@gmail.com>
2018-12-05 16:56:47 +0200

    test: Add validation tests

--- src/streamer/validator/streamer.validator.spec.ts
+++ src/streamer/validator/streamer.validator.spec.ts
@@ -1,23 +1,39 @@
 import { expect } from 'chai';
 import { StreamerValidator } from './streamer.validator';
 import { ValidRequestMocks, responseMock } from './streamer.mocks';
-import { PathInvalidError } from '../../utils/errors/userErrors';
+import { PathInvalidError, RangeHeaderInvalidError } from '../../utils/errors/userErrors';
 
 describe('Streamer Validator Middleware', function () {
-    describe('#isVideoIdValid', function () {
-        it('Should not throw an error', function () {
-            StreamerValidator.canStreamVideo(new ValidRequestMocks().canStreamVideo, responseMock, (error: Error) => {
-                expect(error).to.not.exist;
-            });
-        });
 
-        it('Should throw PathInvalidError when id is invalid', function () {
-            const invalidRequestMock = new ValidRequestMocks().canStreamVideo;
-            invalidRequestMock.params.path = `${'a'.repeat(1025)}.mp4`;
-            StreamerValidator.canStreamVideo(invalidRequestMock, responseMock, (error: Error) => {
-                expect(error).to.exist;
-                expect(error).to.be.instanceOf(PathInvalidError);
-            });
+    describe('#validatePath', function () {
+        it('Should return undefined when path is valid', function () {
+            expect(StreamerValidator.validatePath('video.mp4')).to.be.undefined;
+        });
+        it('Should return PathInvalidError when path is too long', function () {
+            expect(StreamerValidator.validatePath('a'.repeat(1025))).to.be.instanceOf(PathInvalidError);
+        });
+        it('Should return PathInvalidError when path doesn\'t have mp4 extension', function () {
+            expect(StreamerValidator.validatePath('video.avi')).to.be.instanceOf(PathInvalidError);
+        });
+        it('Should return PathInvalidError when path doesn\'t have any extension', function () {
+            expect(StreamerValidator.validatePath('video')).to.be.instanceOf(PathInvalidError);
         });
     });
+
+    describe('#validateRangeHeader', function () {
+        it('Should return undefined when range header is valid', function () {
+            expect(StreamerValidator.validateRangeHeader('bytes=100-2000')).to.be.undefined;
+        });
+        it('Should return undefined when range header is valid but not contains the `end` property', function () {
+            expect(StreamerValidator.validateRangeHeader('bytes=100-')).to.be.undefined;
+        });
+        it('Should return RangeHeaderInvalidError when range header does not contain `start` property', function () {
+            const err = StreamerValidator.validateRangeHeader('bytes=-100')
+            expect(err).to.be.instanceOf(RangeHeaderInvalidError);
+        });
+        it('Should return RangeHeaderInvalidError when range header is not in a bytes format', function () {
+            const err = StreamerValidator.validateRangeHeader('test=0-100')
+            expect(err).to.be.instanceOf(RangeHeaderInvalidError);
+        });
+    })
 });
