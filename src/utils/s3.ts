397009f00fe73592aedca3f0bc9239ea5acf70b3
Ron Borysovski <roni537@gmail.com>
2018-12-05 16:57:02 +0200

    refactor: Export s3 connection instance

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -2,11 +2,13 @@ import * as aws from 'aws-sdk';
 import * as fs from 'fs';
 import { config } from './../config';
 
-const s3: aws.S3 = new aws.S3({
+export const S3Instance: aws.S3 = new aws.S3({
     region: config.s3.region,
     accessKeyId: config.s3.accessKeyId,
     secretAccessKey: config.s3.secretAccessKey,
     signatureVersion: config.s3.signatureVersion,
+    endpoint: config.s3.endpoint,
+    s3ForcePathStyle: true,
 });
 
 const bucket: string = config.s3.bucket;
@@ -20,17 +22,17 @@ export function uploadFile(filePath: string, fileName: string, contentType?: str
     };
 
     if (contentType) putOptions.ContentType = contentType;
-    return s3.putObject(putOptions).promise();
+    return S3Instance.putObject(putOptions).promise();
 }
 
 export function deleteFile(fileName: string) {
-    return s3.deleteObject({ Bucket: bucket, Key: fileName }).promise();
+    return S3Instance.deleteObject({ Bucket: bucket, Key: fileName }).promise();
 }
 
 export function listFiles(videoName?: string) {
     if (videoName) {
-        s3.headObject({ Bucket: bucket, Key: videoName }, (err, data) => err ? console.log(err) : console.log(data));
+        S3Instance.headObject({ Bucket: bucket, Key: videoName }, (err, data) => err ? console.log(err) : console.log(data));
     } else {
-        s3.listObjectsV2({ Bucket: bucket }, (err, data) => err ? console.log(err) : console.log(data));
+        S3Instance.listObjectsV2({ Bucket: bucket }, (err, data) => err ? console.log(err) : console.log(data));
     }
 }
