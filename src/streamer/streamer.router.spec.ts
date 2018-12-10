eb9736de45c750f924ce705ce50ce4c584424ed5
Ron Borysovski <roni537@gmail.com>
2018-12-10 13:33:23 +0200

    test: Cover all end tests

--- src/streamer/streamer.router.spec.ts
+++ src/streamer/streamer.router.spec.ts
@@ -1,10 +1,9 @@
-import { expect } from 'chai';
+import * as fs from 'fs';
 import { sign } from 'jsonwebtoken';
 import * as request from 'supertest';
 import { config } from '../config';
 import { Server } from '../server';
-import { uploadFile, deleteFile, listFiles } from '../utils/s3';
-import * as fs from 'fs';
+import { deleteFile, uploadFile } from '../utils/s3';
 
 const apiEndpoint = '/api/streamer/video';
 const testFilesPath = './src/test-files';
@@ -13,6 +12,8 @@ const { maxChunkSize } = config.streamer;
 describe('Streamer Module', () => {
     let server: Server;
     let videoFileSize: number;
+    const authorizationHeader = `Bearer ${sign({ id: 'user@domain' }, config.authentication.secret)}`;
+
 
     before(async () => {
         await uploadFile(`${testFilesPath}/video.mp4`, 'video.mp4', 'video/mp4');
@@ -33,6 +34,7 @@ describe('Streamer Module', () => {
             it('Should return the whole video file (for downloading)', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .expect('Content-Type', 'video/mp4')
                     .expect('Content-Length', videoFileSize.toString())
                     .expect(200);
@@ -41,6 +43,7 @@ describe('Streamer Module', () => {
             it('Should stream first chunk', async () => {
                 await request(server.app)
                     .get('/api/streamer/video/video.mp4')
+                    .set({ authorization: authorizationHeader })
                     .set('Accept', '*/*')
                     .set('Range', 'bytes=0-')
                     .expect('Content-Type', 'video/mp4')
@@ -52,6 +55,7 @@ describe('Streamer Module', () => {
             it('Should stream middle chunk of video', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .set('Accept', '*/*')
                     .set('Range', `bytes=${videoFileSize / 2}-${(videoFileSize / 2) + maxChunkSize}`)
                     .expect('Content-Type', 'video/mp4')
@@ -63,6 +67,7 @@ describe('Streamer Module', () => {
             it('Should stream last chunk of video', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .set('Accept', '*/*')
                     .set('Range', `bytes=${videoFileSize - 1 - maxChunkSize}-${videoFileSize - 1}`)
                     .expect('Content-Type', 'video/mp4')
@@ -71,41 +76,49 @@ describe('Streamer Module', () => {
                     .expect(206);
             });
 
-            it('Should stream multiple chunks of video', async () => {
+            it('Should stream whole chunks of video', async () => {
 
                 let totalChunkSize = 0;
-                let x = 0;
+                let nextByte = 0;
 
                 while (totalChunkSize < videoFileSize) {
                     await request(server.app)
                         .get(`${apiEndpoint}/video.mp4`)
+                        .set({ authorization: authorizationHeader })
                         .set('Accept', '*/*')
-                        .set('Range', `bytes=${totalChunkSize + x}-`)
+                        .set('Range', `bytes=${totalChunkSize + nextByte}-`)
                         .expect('Content-Type', 'video/mp4')
-                        .expect('Content-Length', `${Math.min(maxChunkSize, videoFileSize - (totalChunkSize + x + 1)) + 1}`)
-                        .expect('content-range', `bytes ${totalChunkSize + x}-${totalChunkSize + x + Math.min(maxChunkSize, videoFileSize - (totalChunkSize + x + 1))}/${videoFileSize}`)
+                        .expect('Content-Length', `${Math.min(maxChunkSize, videoFileSize - (totalChunkSize + nextByte + 1)) + 1}`)
+                        .expect('content-range', `bytes ${totalChunkSize + nextByte}-${totalChunkSize + nextByte + Math.min(maxChunkSize, videoFileSize - (totalChunkSize + nextByte + 1))}/${videoFileSize}`)
                         .expect(206);
 
                     totalChunkSize += Math.min(maxChunkSize, videoFileSize - totalChunkSize);
-                    x = 1;
+                    nextByte = 1;
                 }
+            });
 
-                // const chunksAmount = Math.ceil(videoFileSize / maxChunkSize);
-                // const chunkSize = maxChunkSize;
+            it('Should stream specific chunk size (<maxChunkSize)', async () => {
+                await request(server.app)
+                    .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
+                    .set('Accept', '*/*')
+                    .set('Range', `bytes=500-724`)
+                    .expect('Content-Type', 'video/mp4')
+                    .expect('Content-Length', '225')
+                    .expect('content-range', `bytes 500-724/${videoFileSize}`)
+                    .expect(206);
+            });
 
-                // for (let i = 1; i <= chunksAmount; i++) {
-                //     const start = (i - 1) * chunkSize;
-                //     const end = i * chunkSize;
-
-                //     await request(server.app)
-                //         .get(`${apiEndpoint}/video.mp4`)
-                //         .set('Accept', '*/*')
-                //         .set('Range', `bytes=${start}-${end}`)
-                //         .expect('Content-Type', 'video/mp4')
-                //         .expect('Content-Length', `${end - start + 1}`)
-                //         .expect('content-range', `bytes ${start}-${end}/${videoFileSize}`)
-                //         .expect(206);
-                // }
+            it('Should stream max chunk size (when chunksize>maxChunkSize)', async () => {
+                await request(server.app)
+                    .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
+                    .set('Accept', '*/*')
+                    .set('Range', `bytes=100-${maxChunkSize * 2}`)
+                    .expect('Content-Type', 'video/mp4')
+                    .expect('Content-Length', (maxChunkSize + 1).toString())
+                    .expect('content-range', `bytes 100-${maxChunkSize + 100}/${videoFileSize}`)
+                    .expect(206);
             });
         });
 
@@ -113,6 +126,7 @@ describe('Streamer Module', () => {
             it('Should not stream chunk of video from negative range', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .set('Accept', '*/*')
                     .set('Range', 'bytes=-100000-200000')
                     .expect(416);
@@ -121,6 +135,7 @@ describe('Streamer Module', () => {
             it('Should not stream chunk of video end before start', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .set('Accept', '*/*')
                     .set('Range', 'bytes=20000-10000')
                     .expect(416);
@@ -129,6 +144,7 @@ describe('Streamer Module', () => {
             it('Should not stream chunk of video end equals start', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .set('Accept', '*/*')
                     .set('Range', 'bytes=100000-100000')
                     .expect(416);
@@ -137,14 +153,16 @@ describe('Streamer Module', () => {
             it('Should not stream chunk of video end after max video size', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .set('Accept', '*/*')
-                    .set('Range', 'bytes=100000-2000000')
+                    .set('Range', `bytes=${videoFileSize - 200}-${videoFileSize + 200}`)
                     .expect(416);
             });
 
             it('Should not stream a non video file', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/text.txt`)
+                    .set({ authorization: authorizationHeader })
                     .expect(400);
             });
         });
@@ -153,6 +171,7 @@ describe('Streamer Module', () => {
             it('Should not stream not found video file', async () => {
                 await request(server.app)
                     .get(`${apiEndpoint}/no-video.mp4`)
+                    .set({ authorization: authorizationHeader })
                     .expect(404);
             });
         });
