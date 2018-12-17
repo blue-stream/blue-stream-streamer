c033433d1f56b601fad60fcd35d56452a57a5649
Yuval Assa <assa.yuval107@gmail.com>
2018-12-17 13:19:53 +0200

    test(validator): add thumbnail and preview tests

--- src/streamer/validator/streamer.validator.spec.ts
+++ src/streamer/validator/streamer.validator.spec.ts
@@ -6,17 +6,29 @@ import { config } from '../../config';
 describe('Streamer Validator Middleware', function () {
 
     describe('#validatePath', function () {
-        it('Should return undefined when path is valid', function () {
+        it('Should return undefined when video path is valid', function () {
             expect(StreamerValidator.validatePath('video.mp4', config.sourceType.video)).to.be.undefined;
+        });
+        it('Should return undefined when thumbnail path is valid', function () {
+            expect(StreamerValidator.validatePath('thumbnail.png', config.sourceType.thumbnail)).to.be.undefined;
+        });
+        it('Should return undefined when preview path is valid', function () {
+            expect(StreamerValidator.validatePath('preview.gif', config.sourceType.preview)).to.be.undefined;
         });
         it('Should return PathInvalidError when path is too long', function () {
             expect(StreamerValidator.validatePath('a'.repeat(1025), config.sourceType.video)).to.be.instanceOf(PathInvalidError);
         });
-        it('Should return PathInvalidError when path doesn\'t have mp4 extension', function () {
+        it('Should return PathInvalidError when video path doesn\'t have mp4 extension', function () {
             expect(StreamerValidator.validatePath('video.avi', config.sourceType.video)).to.be.instanceOf(PathInvalidError);
         });
+        it('Should return PathInvalidError when thumbnail path doesn\'t have png extension', function () {
+            expect(StreamerValidator.validatePath('thumbnail.jpg', config.sourceType.thumbnail)).to.be.instanceOf(PathInvalidError);
+        });
+        it('Should return PathInvalidError when preview path doesn\'t have gif extension', function () {
+            expect(StreamerValidator.validatePath('preview.APNG', config.sourceType.preview)).to.be.instanceOf(PathInvalidError);
+        });
         it('Should return PathInvalidError when path doesn\'t have any extension', function () {
-            expect(StreamerValidator.validatePath('video', config.sourceType.video)).to.be.instanceOf(PathInvalidError);
+            expect(StreamerValidator.validatePath('path', config.sourceType.video)).to.be.instanceOf(PathInvalidError);
         });
     });
 
