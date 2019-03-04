bfd71912e2fa153f212fd552a2be2710c3babf8c
Ron Borysovski <roni537@gmail.com>
2019-03-04 15:40:27 +0200

    feat: Add classification middleware

---
+++ src/streamer/streamer.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { config } from '../config';
+import { UnauthorizedResourceError } from '../utils/errors/userErrors';
+
+export function hasRequiredToken(req: Request, res: Response, next: NextFunction) {
+    const videoToken = (req && req.query) ? req.query[config.videoToken.tokenName] : null;
+
+    if (!videoToken) throw new UnauthorizedResourceError();
+
+    const path = req.params.path;
+    const decodedToken = verify(videoToken, config.videoToken.secret) as { userId: string, path: string };
+
+    if (
+        !decodedToken ||
+        decodedToken.path !== path ||
+        decodedToken.userId !== req.user.id
+    ) throw new UnauthorizedResourceError();
+
+    return next();
+}
